d3cec7f278dabfc4c2bec2a36b57cfab088ec681
Yvonne <tailcloud14916@gmail.com>
2017-02-18 13:27:01 +0800

    make the result of searching more specific and clear

--- mojimLyrics.py
+++ mojimLyrics.py
@@ -5,33 +5,26 @@ from urllib2 import Request, urlopen, URLError
 from bs4 import BeautifulSoup
 import urllib, urllib2
 import sys
+import re
 
 def findLyrics(NameofSong):
 	request = urllib2.urlopen('https://mojim.com/'+NameofSong+'.html?u3')
 	soup = BeautifulSoup(request,"html.parser")
-	for article in soup.findAll('a', title=True):
-	    print "Title: "+article['title']
-	    print "URL: http://mojim.com"+article['href']
-	try:
-		response = urlopen(request)
-		result = response.read()
-		print result
-	except URLError, e:
-	    print 'No kittez. Got an error code:', e
-      
+	spans = soup('span',{'class':'mxsh_ss4'})
+	for span in spans:
+		spanas = span.findAll('a')
+		for spana in spanas:
+			cont = spana['title']
+			print 'Title: '+ spana['title']
+			print 'Url: http://mojim.com'+ spana['href']
 def downloadLyrics(Url):
 	request = urlopen(Url)
 	soup = BeautifulSoup(request,"html.parser")
 	for article in soup.find('dd',{'class':'fsZx3'}):
 		print article.extract().string
-	try:
-		response = urlopen(request)
-		result = response.read()
-		print result
-	except URLError, e:
-	    print 'No kittez. Got an error code:', e
-      
 def main(argv1,argv2):
+	print argv1
+	print argv2
 	if argv1 == 'find':
 		findLyrics(argv2)
 	if argv1 == 'download':
@@ -40,8 +33,8 @@ def main(argv1,argv2):
 if __name__=="__main__": 
 	print '*********************************************'
 	print '* If you want to search the lyricsfinder,   *'
-	print '* Usage: python mojimLyrics.py find Ma-yu  *'
+	print '* Usage: python lyricsfinder.py find Ma-yu  *'
 	print '* If you want to download it,               *'            
-	print '* Usage: python mojimLyrics.py download url*'
+	print '* Usage: python lyricsfinder.py download url*'
 	print '*********************************************'
 	main(sys.argv[1],sys.argv[2])
